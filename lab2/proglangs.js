de9da7d4a442022ff878ebc61f00b710333023ba
huseia13 <houssein24@gmail.com>
2013-10-10 10:17:28 +0200

    Lagde egen mappe for hver lab
    Har fiksa koden på lab1
    Lab2 har jeg ikke gjort noen siden Janis sa det var greit, skal ta
    kontakt om det :)
    Tilslutt har jeg lagt til lab 3 filene :)

--- lab2/proglangs.js
+++ lab2/proglangs.js
@@ -5,8 +5,26 @@ var textBlocks = new Array(
 'Du har selektert Cobol.','Du har selektert ColdFusion.','Du har selektert Erlang.','Du har selektert Fortrean.','Du har selektert Haskell.',
 'Du har selektert Java.','Du har selektert Javascript.','Du har selektert Lisp.','Du har selektert Perl.','Du har selektert Python.',
 'Du har selektert Ruby.');
-
 function changeText(elemid) { 
 var ind = document.getElementById(elemid).selectedIndex; 
-document.getElementById("text").innerHTML=textBlocks[ind]; 
+document.getElementById("display").innerHTML=textBlocks[ind]; 
 } 
+
+function AlertIt() {
+var answer = confirm ("Kronologisk side er under bygning.")
+if (answer)
+window.location="proglangs.html";
+}
+
+function AlertIt1() {
+var answer = confirm ("Hierarkisk side er under bygning.")
+if (answer)
+window.location="proglangs.html";
+}
+
+function AlertIt2() {
+var answer = confirm ("Alphapetisk side er under bygning.")
+if (answer)
+window.location="proglangs.html";
+}
+
