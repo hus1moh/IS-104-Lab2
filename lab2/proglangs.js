e806037b2f7dc12d4eacbebc5e18f3e00d4a8158
huseia13 <houssein24@gmail.com>
2013-10-11 17:08:43 +0200

    Retta på v2 og v3 på lab3 og har fiksa på lab2 nå

--- lab2/proglangs.js
+++ lab2/proglangs.js
@@ -1,30 +1,10 @@
-var textBlocks = new Array( 
-'Select from the list to change this box', 
-'Du har selektert AppleScript.', 
-'Du har selektert Asp.','Du har selektert Basic.','Du har selektert C.','Du har selektert C++.','Du har selektert Clojure.',
-'Du har selektert Cobol.','Du har selektert ColdFusion.','Du har selektert Erlang.','Du har selektert Fortrean.','Du har selektert Haskell.',
-'Du har selektert Java.','Du har selektert Javascript.','Du har selektert Lisp.','Du har selektert Perl.','Du har selektert Python.',
-'Du har selektert Ruby.');
-function changeText(elemid) { 
-var ind = document.getElementById(elemid).selectedIndex; 
-document.getElementById("display").innerHTML=textBlocks[ind]; 
-} 
+// finner frem fra dropdown list og vise verdien til den selekterte
+list.onchange = function textoutput() {	
+	var outputbox = document.getElementById('outputbox');
+outputbox.innerHTML = "Du har valgt:  " + this.value;} 
 
-function AlertIt() {
-var answer = confirm ("Kronologisk side er under bygning.")
-if (answer)
-window.location="proglangs.html";
-}
-
-function AlertIt1() {
-var answer = confirm ("Hierarkisk side er under bygning.")
-if (answer)
-window.location="proglangs.html";
-}
-
-function AlertIt2() {
-var answer = confirm ("Alphapetisk side er under bygning.")
-if (answer)
-window.location="proglangs.html";
-}
-
+// popupfunksjon som viser at sidene er under konstruksjon
+function konstruksjon ()
+	{
+	alert("Obs Siden er under konstruksjon")
+	}
